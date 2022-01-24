acbc1770799c053e3b4dce21d50a749a45337220
Jaxcoder <codenamejason@outlook.com>
2022-01-24 11:38:40 -0500

    finish challenge 0

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -8,9 +8,7 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const chainId = await getChainId();
 
   await deploy("YourCollectible", {
-    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
-    // args: [ "Hello", ethers.utils.parseEther("1.5") ],
     log: true,
   });
 
@@ -18,19 +16,18 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const yourCollectible = await ethers.getContract("YourCollectible", deployer);
 
   // ToDo: Verify your contract with Etherscan for public chains
-  // if (chainId !== "31337") {
-  //   try {
-  //     console.log(" 🎫 Verifing Contract on Etherscan... ");
-  //     await sleep(3000); // wait 3 seconds for deployment to propagate bytecode
-  //     await run("verify:verify", {
-  //       address: yourCollectible.address,
-  //       contract: "contracts/YourCollectible.sol:YourCollectible",
-  //       // contractArguments: [yourToken.address],
-  //     });
-  //   } catch (e) {
-  //     console.log(" ⚠️ Failed to verify contract on Etherscan ");
-  //   }
-  // }
+  if (chainId !== "31337") {
+    try {
+      console.log(" 🎫  Verifing Contract on Etherscan... ");
+      await sleep(6000); // wait 3 seconds for deployment to propagate bytecode
+      await run("verify:verify", {
+        address: yourCollectible.address,
+        contract: "contracts/YourCollectible.sol:YourCollectible",
+      });
+    } catch (e) {
+      console.log(" ⚠️ Failed to verify contract on Etherscan ==> ", e);
+    }
+  }
 };
 
 function sleep(ms) {
